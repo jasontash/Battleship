9dce3bee19340e25ccf814c2628dbe99b64b4634
maddySwin <9993908@student.swin.edu.au>
2015-09-24 16:33:40 +1000

    add an easy difficulty mode and allow medium mode to be set

--- src/MenuController.cs
+++ src/MenuController.cs
@@ -287,10 +287,10 @@ namespace Battleship
 		{
 			switch (button) {
 				case SETUP_MENU_EASY_BUTTON:
-					GameController.SetDifficulty(AIOption.Hard);
+					GameController.SetDifficulty(AIOption.Easy);
 					break;
 				case SETUP_MENU_MEDIUM_BUTTON:
-					GameController.SetDifficulty(AIOption.Hard);
+					GameController.SetDifficulty(AIOption.Medium);
 					break;
 				case SETUP_MENU_HARD_BUTTON:
 					GameController.SetDifficulty(AIOption.Hard);
---
+++ src/Model/AIEasyPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Battleship
+{
+	public class AIEasyPlayer: AIPlayer
+	{
+		public AIEasyPlayer (BattleShipsGame controller): base(controller)
+		{
+		}
+
+		/// <summary>
+		/// Generate a valid, random row and column to shoot at
+		/// </summary>
+		/// <param name="row">output the row for the next shot</param>
+		/// <param name="column">output the column for the next show</param>
+		protected override void GenerateCoords (ref int row, ref int column)
+		{
+			do 
+			{
+				SearchCoords (ref row, ref column);
+			} while ((row < 0 || column < 0 || row >= EnemyGrid.Height || column >= EnemyGrid.Width || EnemyGrid[row, column] != TileView.Sea));
+			//while inside the grid and not a sea tile do the search
+		}
+
+		/// <summary>
+		/// SearchCoords will randomly generate shots within the grid as long as its not hit that tile already
+		/// </summary>
+		/// <param name="row">Row.</param>
+		/// <param name="column">Column.</param>
+		private void SearchCoords (ref int row, ref int column)
+		{
+			row = _Random.Next(0, EnemyGrid.Height);
+			column = _Random.Next(0, EnemyGrid.Width);
+		}
+
+		/// <summary>
+		/// Process shot will throw an exception in the case of AI error resulting in a square being hit twice
+		/// </summary>
+		/// <param name="result">The result of the shot</param>
+		/// <param name="row">the row shot</param>
+		/// <param name="col">the column shot</param>
+		protected override void ProcessShot(int row, int col, AttackResult result)
+		{
+			if (result.Value == ResultOfAttack.ShotAlready)
+			{
+				throw new ApplicationException("Error in AI");
+			}
+		}
+
+	}
+}
+
